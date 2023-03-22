43628d55969e2819d0b94cb688bc27c06401787b
Minsu-Jung1999 <96643903+Minsu-Jung1999@users.noreply.github.com>
2023-03-21 23:50:42 -0700

    3-21

---
+++ Source/TPSProject/private/TPSProjectGameModeBase.cpp
@@ -0,0 +1,9 @@
+#include "TPSProjectGameModeBase.h"
+#include "TPSProject.h"
+
+
+
+ATPSProjectGameModeBase::ATPSProjectGameModeBase()
+{
+	PRINT_LOG(TEXT("My Log: %s"), TEXT("TPS project!!"));
+}
---
+++ Source/TPSProject/public/TPSProject.h
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(TPS, Log, All);
+
+#define CALLINFO (FString(__FUNCTION__) + TEXT("(") + FString::FromInt(__LINE__) + TEXT(")"))
+
+#define PRINT_CALLINFO() UE_LOG(TPS, Warning, TEXT("%s"), *CALLINFO)
+
+/// (...) 가변 인수 인수를 여러 개 넘겨받을 수 있다.
+#define PRINT_LOG(fmt, ...) UE_LOG(TPS, Warning, TEXT("%s,%s"), *CALLINFO, \
+*FString::Printf(fmt, ##__VA_ARGS__))
+
